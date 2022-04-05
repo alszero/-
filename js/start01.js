7d2e02227540b88fe1975585e0a0656d2e8afe15
Seungbeen <seungbin803@gmail.com>
2022-04-06 07:55:06 +0900

    Q&A 수정

--- js/start01.js
+++ js/start01.js
@@ -4,29 +4,29 @@ const qna = document.querySelector("#qna");
 const endPoint = 12;
 const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-function calResult(){
-    var result = select.indexOf(Math.max(...select));
-    return result;
-  }
-  
-  function setResult(){
-    let point = calResult();
-    const resultName = document.querySelector('.resultname');
-    resultName.innerHTML = infoList[point].name;
-  
-    var resultImg = document.createElement('img');
-    const imgDiv = document.querySelector('#resultImg');
-    var imgURL = 'img/image-' + point + '.png';
-    resultImg.src = imgURL;
-    resultImg.alt = point;
-    resultImg.classList.add('img-fluid');
-    imgDiv.appendChild(resultImg);
-  
-    const resultDesc = document.querySelector('.resultDesc');
-    resultDesc.innerHTML = infoList[point].desc;
-  }
-  
-function goResult(){
+function calResult() {
+  var result = select.indexOf(Math.max(...select));
+  return result;
+}
+
+function setResult() {
+  let point = calResult();
+  const resultName = document.querySelector(".resultname");
+  resultName.innerHTML = infoList[point].name;
+
+  var resultImg = document.createElement("img");
+  const imgDiv = document.querySelector("#resultImg");
+  var imgURL = "img/image-" + point + ".png";
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add("img-fluid");
+  imgDiv.appendChild(resultImg);
+
+  const resultDesc = document.querySelector(".resultDesc");
+  resultDesc.innerHTML = infoList[point].desc;
+}
+
+function goResult() {
   qna.style.WebkitAnimation = "fadeOut 1s";
   qna.style.animation = "fadeOut 1s";
   setTimeout(() => {
@@ -34,53 +34,58 @@ function goResult(){
     result.style.animation = "fadeIn 1s";
     setTimeout(() => {
       qna.style.display = "none";
-      result.style.display = "block"
-    }, 450)})
-    setResult();
+      result.style.display = "block";
+    }, 450);
+  });
+  setResult();
 }
 
-function addAnswer(answerText, qIdx){
-  let a = document.querySelector('.answerBox');
-  let answer = document.createElement('button');
-  answer.classList.add('answerList');
-  answer.classList.add('my-3');
-  answer.classList.add('py-3');
-  answer.classList.add('mx-auto');
-  answer.classList.add('fadeIn');
+function addAnswer(answerText, qIdx) {
+  let a = document.querySelector(".answerBox");
+  let answer = document.createElement("button");
+  answer.classList.add("answerList");
+  answer.classList.add("my-3");
+  answer.classList.add("py-3");
+  answer.classList.add("mx-auto");
+  answer.classList.add("fadeIn");
 
   // appendChild함수를 통해 answer가 a에게 소속될 수 있도록 관계를 만들어줌
   a.appendChild(answer);
   answer.innerHTML = answerText;
 
-  answer.addEventListener("click", function(){
-    let children = document.querySelectorAll('.answerList');
-    for(let i = 0; i < children.length; i++){
-      children[i].disabled = true;
-      children[i].style.WebkitAnimation = "fadeOut 0.5s";
-      children[i].style.animation = "fadeOut 0.5s";
-    }
-    setTimeout(() => {
-      for(let i = 0; i < children.length; i++){
-        children[i].style.display = 'none';
+  answer.addEventListener(
+    "click",
+    function () {
+      let children = document.querySelectorAll(".answerList");
+      for (let i = 0; i < children.length; i++) {
+        children[i].disabled = true;
+        children[i].style.WebkitAnimation = "fadeOut 0.5s";
+        children[i].style.animation = "fadeOut 0.5s";
       }
-      goNext(++qIdx);
-    },450)
-  }, false);
+      setTimeout(() => {
+        for (let i = 0; i < children.length; i++) {
+          children[i].style.display = "none";
+        }
+        goNext(++qIdx);
+      }, 450);
+    },
+    false
+  );
 }
 
-function goNext(qIdx){
-  let q = document.querySelector('.qBox');
+function goNext(qIdx) {
+  let q = document.querySelector(".qBox");
   // innerHTML은 요소(element)내에 포함 된 HTML 또는 XML마크업을
   // 가져오거나 설정합니다.
   q.innerHTML = qnaList[qIdx].q;
-  for (let i in qnaList[qIdx].a){
+  for (let i in qnaList[qIdx].a) {
     addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
   // let status = document.querySelector('.statusBar');
   // status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
 
-function begin(){
+function begin() {
   main.style.WebkitAnimation = "fadeOut 1s";
   main.style.animation = "fadeOut 1s";
   setTimeout(() => {
@@ -88,8 +93,8 @@ function begin(){
     qna.style.animation = "fadeIn 1s";
     setTimeout(() => {
       main.style.display = "none";
-      qna.style.display = "block"
-    }, 450)
+      qna.style.display = "block";
+    }, 450);
     let qIdx = 0;
     goNext(qIdx);
   }, 450);
